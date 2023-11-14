53da0de9a1be17b82be9672816d7606c35096265
Alain Benbassat <alain@businessandcode.eu>
2023-11-14 15:31:52 +0100

    new newsletters fields

--- CRM/Civienketo/lib/newsletter.php
+++ CRM/Civienketo/lib/newsletter.php
@@ -3,69 +3,75 @@
   Manage newsletter
 */
 
-define("NEWSLETTER_GROUP", 42);
-define("ACTUBRUX", 317);
-define("ACTUBW", 318);
-define("ACTUHainaut", 319);
-define("ACTULiège", 316);
-define("ACTULUX", 315);
-define("ACTUNAM", 284);
-
 /***
   Subscribe a contact uid to the newsletter
 */
 function subscribe_to_newsletter($uid) {
-  $result = civicrm_api3('groupContact', 'create', array(
-      'sequential' => 1,
-      'contact_id' => $uid,
-      'group_id' => NEWSLETTER_GROUP,
-  ));
-
-  return $result;
+  \Civi\Api4\Contact::update(FALSE)
+    ->addValue('Newsletters.Newsletter_g_n_rale', 1)
+    ->addWhere('id', '=', $uid)
+    ->execute();
 }
 
 /***
   Subscribe a contact uid to the Actu newsletter
 */
 function subscribe_to_regional_newsletter($uid, $country, $postalcode) {
-  if (($country!='BE') or ($postalcode == NULL)) return;
- 
-  if (($postalcode > 0) and ($postalcode < 1299)) {
-    $group = ACTUBRUX;               // 'Bruxelles';
-  } else if ($postalcode < 1499) {
-    $group = ACTUBW;               // 'Brabant Wallon';
-  } else if ($postalcode < 1999) {
-    $group = NULL;               // 'Vlaams-Brabant';
-  } else if ($postalcode < 2999) {
-    $group = NULL;               // 'Antwerpen';
-  } else if ($postalcode < 3499) {
-    $group = NULL;               // 'Vlaams-Brabant';
-  } else if ($postalcode < 3999) {
-    $group = NULL;               // 'Limbourg';
-  } else if ($postalcode < 4999) {
-    $group = ACTULiège;               // 'Liège';
-  } else if ($postalcode < 5999) {
-    $group = ACTUNAM;               // 'Namur';
-  } else if ($postalcode < 6599) {
-    $group = ACTUHainaut;               // 'Hainaut';
-  } else if ($postalcode < 6999) {
-    $group = ACTULUX;               // 'Luxembourg'; 
-  } else if ($postalcode < 7999) {
-    $group = ACTUHainaut;               // 'Hainaut';
-  } else if ($postalcode < 8999) {
-    $group = NULL;               // 'West-Vlaanderen';
-  } else if ($postalcode < 9999) {
-    $group = NULL;               // Oost-Vlanderen
-  } else
-    $group = NULL;
-
-  if ($group != NULL) {
-    $result = civicrm_api3('groupContact', 'create', array(
-        'sequential' => 1,
-        'contact_id' => $uid,
-        'group_id' => $group,
-    ));
+  if (($country!='BE') or ($postalcode == NULL)) {
+    return;
   }
-  return $result;
 
+  $field = '';
+  if (($postalcode > 0) and ($postalcode < 1299)) {
+    // Bruxelles
+    $field = 'Agendas_r_gionaux.ActuBrux';
+  }
+  elseif ($postalcode < 1499) {
+    // Brabant Wallon
+    $field = 'Agendas_r_gionaux.ActuBW';
+  }
+  elseif ($postalcode < 1999) {
+    // Vlaams-Brabant
+  }
+  elseif ($postalcode < 2999) {
+    // Antwerpen
+  }
+  elseif ($postalcode < 3499) {
+    // Vlaams-Brabant
+  }
+  elseif ($postalcode < 3999) {
+    // Limbourg
+  }
+  elseif ($postalcode < 4999) {
+    // Liège
+    $field = 'Agendas_r_gionaux.ActuLi_ge';
+  }
+  elseif ($postalcode < 5999) {
+    // Namur
+    $field = 'Agendas_r_gionaux.ActuNam';
+  }
+  elseif ($postalcode < 6599) {
+    // Hainaut
+    $field = 'Agendas_r_gionaux.ActuHainaut';
+  }
+  elseif ($postalcode < 6999) {
+    // Luxembourg
+    $field = 'Agendas_r_gionaux.ActuLux';
+  }
+  elseif ($postalcode < 7999) {
+    $field = 'Agendas_r_gionaux.ActuHainaut';
+  }
+  elseif ($postalcode < 8999) {
+    // West-Vlaanderen
+  }
+  elseif ($postalcode < 9999) {
+    // Oost-Vlanderen
+  }
+
+  if ($field) {
+    \Civi\Api4\Contact::update(FALSE)
+      ->addValue($field, 1)
+      ->addWhere('id', '=', $uid)
+      ->execute();
+  }
 }
